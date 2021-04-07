5539a8e46a491bbfd3f9b4d678eb8105e310a77d
ClaraCoding <clara.beuzart@gmail.com>
2021-04-07 18:26:46 +0200

    v2 = mise à jour des quantités pour un même plat

--- src/main/java/com/natixis/restaurant/entity/Plat.java
+++ src/main/java/com/natixis/restaurant/entity/Plat.java
@@ -47,16 +47,66 @@ public class Plat {
     // Enregistrement d'un plat dans une facture = création d'une nouvelle ligne dans la table plat_facture de la base de donnée
     public static void savePlatFacture(Connection connection, int key, int plat, int quantity) throws SQLException {
 
-        String insertCmd = "INSERT INTO plat_facture_combo (plat_idx, facture_idx, quantity) VALUES( ?, ?, ?);";
+        // Récupération du nom et du prix du plat => si le menu change (prix/plat), il faut garder cette information "en dur" dans la facture
 
-        try (PreparedStatement stmt = connection.prepareStatement(insertCmd)) {
+        // Selection du nom et du prix du plat
+        String sqlTypePlat = ("SELECT nom, prix FROM plat WHERE id=?");
+        String platCmd = "";
+        double platPrix = 0;
+
+        try (PreparedStatement stmt = connection.prepareStatement(sqlTypePlat)) {
             stmt.setInt(1, plat);
-            stmt.setInt(2, key);
-            stmt.setInt(3, quantity);
+            ResultSet result = stmt.executeQuery();
+            while (result.next()) {
+                platCmd = result.getString("nom");
+                platPrix = result.getDouble("prix");
+            }
 
-            stmt.execute();
+            System.out.println(platCmd + " " + quantity + " x " + platPrix + "€");
+        }
+
+        // #V2 = mise à jour quantité
+        //Vérification si le plat existe déjà pour la facture en cours
+        String sqlFacturePlat = ("SELECT plat_nom, quantity FROM plat_facture_combo WHERE facture_idx =? AND plat_nom=?");
+        String platVerif = "";
+
+        try (PreparedStatement stmt = connection.prepareStatement(sqlFacturePlat)) {
+            stmt.setInt(1, key);
+            stmt.setString(2, platCmd);
+            ResultSet result = stmt.executeQuery();
+            while (result.next()) {
+                platVerif = result.getString("plat_nom");
+                int qty = result.getInt("quantity");
+                quantity = quantity + qty;
+            }
+
+            if (platVerif.isEmpty()) {
+
+                // Si non = Ajout de la ligne
+                String insertCmd = "INSERT INTO plat_facture_combo (facture_idx, quantity, plat_nom, plat_prix) VALUES( ?, ?, ?, ?);";
+
+                try (PreparedStatement stmtn = connection.prepareStatement(insertCmd)) {
+                    stmtn.setInt(1, key);
+                    stmtn.setInt(2, quantity);
+                    stmtn.setString(3, platCmd);
+                    stmtn.setDouble(4, platPrix);
+
+                    stmtn.execute();
+                }
+            } else { // Si oui = mise à jour de la ligne
+                String insertCmd = "UPDATE plat_facture_combo SET quantity = ? WHERE facture_idx=? AND plat_nom=?";
+
+                try (PreparedStatement stmtu = connection.prepareStatement(insertCmd)) {
+                    stmtu.setInt(1, quantity);
+                    stmtu.setInt(2, key);
+                    stmtu.setString(3, platCmd);
+
+                    stmtu.execute();
+                }
+            }
         }
     }
 }
+
 
 
